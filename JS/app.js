6e09bbb004749ce24012899a6b1e6b8e9fad7b32
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2023-11-30 15:51:47 -0300

    Realice la logica, agregue estilos y modifique el html

--- JS/app.js
+++ JS/app.js
@@ -5,25 +5,79 @@ const botonReset = document.getElementById(`btnReset`);
 let contadorSegundos = 0;
 let contadorMinutos = 0;
 let contadorHoras = 0;
+let setIntervalCronometrar;
 botonPausar.disabled = true;
-botonDetener.disabled = true;
+botonReset.disabled = true;
 
 function cronometrar() {
-  setInterval(() => {
-    if (contadorSegundos < 60) {
-      contadorSegundos++;
-    } else if (contadorMinutos < 60) {
+  setIntervalCronometrar = setInterval(() => {
+    contadorSegundos++;
+    if (contadorSegundos >= 60) {
       contadorMinutos++;
       contadorSegundos = 0;
-    } else {
-      contadorHoras++;
-      contadorMinutos = 0;
+      if (contadorMinutos >= 60) {
+        contadorHoras++;
+        contadorMinutos = 0;
+      }
     }
 
-    if(contadorSegundos < 10){
-        cronometro.innerHTML = `0${contadorHoras} : 0${contadorMinutos} : 0${contadorSegundos}`;
+    if (contadorHoras < 10) {
+      if (contadorMinutos < 10) {
+        if (contadorSegundos < 10) {
+          cronometro.innerHTML = `0${contadorHoras} : 0${contadorMinutos} : 0${contadorSegundos}`;
+        } else {
+          cronometro.innerHTML = `0${contadorHoras} : 0${contadorMinutos} : ${contadorSegundos}`;
+        }
+      } else {
+        if (contadorSegundos < 10) {
+          cronometro.innerHTML = `0${contadorHoras} : ${contadorMinutos} : 0${contadorSegundos}`;
+        } else {
+          cronometro.innerHTML = `0${contadorHoras} : ${contadorMinutos} : ${contadorSegundos}`;
+        }
+      }
     } else {
-        cronometro.innerHTML = `0${contadorHoras} : 0${contadorMinutos} : ${contadorSegundos}`
+      if (contadorMinutos < 10) {
+        if (contadorSegundos < 10) {
+          cronometro.innerHTML = `${contadorHoras} : 0${contadorMinutos} : 0${contadorSegundos}`;
+        } else {
+          cronometro.innerHTML = `0${contadorHoras} : 0${contadorMinutos} : ${contadorSegundos}`;
+        }
+      } else {
+        if (contadorSegundos < 10) {
+          cronometro.innerHTML = `${contadorHoras} : ${contadorMinutos} : 0${contadorSegundos}`;
+        } else {
+          cronometro.innerHTML = `${contadorHoras} : ${contadorMinutos} : ${contadorSegundos}`;
+        }
+      }
     }
-  });
+  }, 1000);
 }
+
+botonIniciar.addEventListener("click", () => {
+  botonIniciar.disabled = true;
+  botonPausar.disabled = false;
+  botonReset.disabled = false;
+  cronometrar();
+});
+
+botonPausar.addEventListener("click", () => {
+  if (botonPausar.innerHTML === "Reanudar") {
+    cronometrar();
+    botonPausar.innerHTML = `Pausar`;
+  } else {
+    clearInterval(setIntervalCronometrar);
+    botonPausar.innerHTML = "Reanudar";
+  }
+});
+
+botonReset.addEventListener("click", () => {
+  botonIniciar.disabled = false;
+  botonPausar.disabled = true;
+  botonReset.disabled = true;
+  clearInterval(setIntervalCronometrar);
+  cronometro.innerHTML = `00 : 00 : 00`;
+  contadorHoras = 0;
+  contadorMinutos = 0;
+  contadorSegundos = 0;
+  botonPausar.innerHTML = `Pausar`;
+});
